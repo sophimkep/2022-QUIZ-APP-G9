0745db8d0c8de5278a656548fb04e0ada728194e
phandyphorn <90320702+phandyphorn@users.noreply.github.com>
2022-01-23 19:43:56 +0700

    #1 add question

--- js/main.js
+++ js/main.js
@@ -1,8 +1,33 @@
+// add question
+function addQuestion() {
+    let questionInput = document.querySelector("#question").value;
+    let firstA = document.querySelector("#answer1").value;
+    let secondA = document.querySelector("#answer2").value;
+    let thirdA = document.querySelector("#answer3").value;
+    let fourthA = document.querySelector("#answer4").value;
 
+   
 
-// display question
-function displayQuestion(){
-    questionList
+    listOfQuestion.push(questionInput);
+
+    let listOfAnswer = [];
+    listOfAnswer.push(firstA)
+    listOfAnswer.push(secondA)
+    listOfAnswer.push(thirdA)
+    listOfAnswer.push(fourthA)
+    listOfListAnswer.push(listOfAnswer)
+    console.log(listOfQuestion)
+    console.log(listOfListAnswer)
+    
+    
 }
-// edit queston
+let listOfListAnswer = []
+let listOfQuestion = [];
+
+
+let createButton = document.getElementById("create-btn");
+createButton.addEventListener("click", addQuestion);
+
+let createQ = document.getElementById("create");
+createQ.addEventListener("click", addQuestion);
  
