dfa0ffc94fdf741193796339abf6c37d3d073698
phandyphorn <90320702+phandyphorn@users.noreply.github.com>
2022-01-24 12:18:09 +0700

    #1 add question

--- js/main.js
+++ js/main.js
@@ -1,33 +1,39 @@
 // add question
+let listOfQuestionAndAnswer= [
+    { Question: 'How many studets of generation 2020-2022 ?', Option1: 70, Option2: 71, Option3: 69, Option4: 65,checked:false},
+    { Question: 'When pnc was created ?', Option1: 2002, Option2: 2005, Option3: 2007, Option4: 2006,checked:true},
+    { Question: 'How many countries that PN operating nowaday  ?', Option1: 3, Option2: 6, Option3: 9, Option4: 5,checked:false}
+]
+
+
+
 function addQuestion() {
+    let newList = {};
     let questionInput = document.querySelector("#question").value;
     let firstA = document.querySelector("#answer1").value;
     let secondA = document.querySelector("#answer2").value;
     let thirdA = document.querySelector("#answer3").value;
     let fourthA = document.querySelector("#answer4").value;
+    let correctAnswer = false;
+    let allAnswers = document.getElementsByName('eachAnswer')
+    for (let answer in allAnswers) {
+        if (allAnswers[answer].checked == true) {
+            correctAnswer= true;
+        }
+    }
 
-   
-
-    listOfQuestion.push(questionInput);
-
-    let listOfAnswer = [];
-    listOfAnswer.push(firstA)
-    listOfAnswer.push(secondA)
-    listOfAnswer.push(thirdA)
-    listOfAnswer.push(fourthA)
-    listOfListAnswer.push(listOfAnswer)
-    console.log(listOfQuestion)
-    console.log(listOfListAnswer)
-    
+    newList.Question = questionInput;
+    newList.Option1 = firstA;
+    newList.Option2 = secondA;
+    newList.Option3 = thirdA;
+    newList.Option4 = fourthA;
+    newList.checked = correctAnswer;
+    listOfQuestionAndAnswer.push(newList)
     
 }
-let listOfListAnswer = []
-let listOfQuestion = [];
-
 
 let createButton = document.getElementById("create-btn");
 createButton.addEventListener("click", addQuestion);
 
-let createQ = document.getElementById("create");
-createQ.addEventListener("click", addQuestion);
+
  
